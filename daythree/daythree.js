851e7ce2d081e5b2ce7b7c9aa3068fea8bc4a8f9
harshadabarve76 <harshadabarve76@gmail.com>
2023-04-17 10:28:10 +0530

    first commit

---
+++ daythree/daythree.js
@@ -0,0 +1,36 @@
+//SINGLE COMMENT
+/*MULTI-LINE COMMENT*/
+/*alert("Congratulations!")
+alert("Hello");
+alert(4+2)
+let num=15;
+    alert(num)
+const     x=12;
+alert(x);
+x=15;
+alert(x);
+var       y=35
+alert(y);*/
+let a=2
+  //assingment operator
+    a*=5;
+    alert(a)
+
+    //relational operator
+    var res=12==0;
+    alert(res)
+
+    //logical operator
+   var y=5;
+   var x=2;
+    alert(x==y && x>y)
+
+    if(x>y)
+      alert("harshada")
+    else
+    alert("barve")
+
+    alert(typeof(true));
+
+    let age=prompt("what's your age",100)
+    alert("your age is :"+age)
